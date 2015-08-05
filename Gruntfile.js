492519ff780fc6e58e681dc7ab25570371f3a88f
Valentin Vago <zeropaper@irata.ch>
2015-08-05 10:46:58 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+'use strict';
+/*jshint node:true*/
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
+  var setup = grunt.file.readJSON(__dirname + '/package.json').setup;
+
+  grunt.initConfig({
+    less: {
+      options: {
+        paths: ['node_modules']
+      },
+      styles: {
+        files: [{
+          src: ['styles/docs.less'],
+          dest: setup.target + '/static/css/docs.css'
+        }]
+      }
+    },
+
+    browserify: {
+      scripts: {
+        files: [{
+          src: ['scripts/index.js'],
+          dest: setup.target + '/static/js/docs.js'
+        }]
+      }
+    },
+
+    watch: {
+      styles: {
+        files: ['styles/**/*.less'],
+        tasks: ['less:styles']
+      },
+      scripts: {
+        files: ['scripts/**/*.js'],
+        tasks: ['browserify:scripts']
+      }
+    }
+  });
+
+  grunt.registerTask('build', ['less:styles', 'browserify:scripts']);
+
+  grunt.registerTask('default', ['build', 'watch']);
+};
---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+'use strict';
+/*jshint browser: true*/
+
+/*global console: false*/
+(function () {
+  console.info('Tada');
+})();
