a01372a16660dd73503b063aefb46af0a902443b
Valentin Vago <zeropaper@irata.ch>
2015-08-12 13:11:11 +0200

    impr(styles): make the get-code instructions collapsible

--- scripts/index.js
+++ scripts/index.js
@@ -60,9 +60,6 @@ function openParentItem(childItem, className) {
 
 
 
-
-
-
 var toc = query('#TableOfContents');
 var navBar = query('.navbar-fixed-top');
 var tocWrapper;
@@ -160,3 +157,13 @@ if (location.hash) {
   shiftWindow();
 }
 window.addEventListener('hashchange', shiftWindow);
+
+
+queryAll('.gs-download-step-panel').forEach(function (panel) {
+  var btn = query('.toggle-instructions', panel);
+  if (!btn) { return; }
+  btn.addEventListener('click', function () {
+    panel.classList.toggle('open');
+  });
+});
+
